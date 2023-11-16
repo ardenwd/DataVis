136bb9f4380e839c3095bf8632d38dc33d5950ff
ardipi <ardendavis2@gmail.com>
2023-11-16 16:33:36 -0500

    major updates lab6

--- lab6/activity_3 copy/main.js
+++ lab6/activity_3 copy/main.js
@@ -1,17 +1,31 @@
+var toolTip = d3.tip()
+.attr("class", "d3-tip")
+.offset([-12, 0])
+.html(function(event, d) {
+    // Inject html, when creating your html I recommend editing the html within your index.html first
+    return "<h5>"+d['name']+"</h5><table><thead><tr><td>Year</td><td>Displacement (cc)</td><td>Weight (lb)</td></tr></thead>"
+         + "<tbody><tr><td>"+d['year']+"</td><td>"+d['displacement (cc)']+"</td><td>"+d['weight (lb)']+"</td></tr></tbody>"
+         + "<thead><tr><td>0-60 mph (s)</td><td>Economy (mpg)</td><td>Cylinders</td><td>Power (hp)</td></tr></thead>"
+         + "<tbody><tr><td>"+d['0-60 mph (s)']+"</td><td>"+d['economy (mpg)']+"</td><td>"+d['cylinders']+"</td><td>"+d['power (hp)']+"</td></tr></tbody></table>"
+});
+
+var svg = d3.select('svg');
+
+svg.call(toolTip);
+
 function onCategoryChanged() {
     var select = d3.select('#xSelector').node();
     // Get current value of select element
     var x = select.options[select.selectedIndex].value;
-    //console.log(x);
+    var xIndex = select.options[select.selectedIndex].index;
+   // console.log(xIndex);
     select = d3.select('#ySelector').node();
     var y = select.options[select.selectedIndex].value;
+    var yIndex = select.options[select.selectedIndex].index;
     // Update chart with the selected category of letters
-    updateChart(x,y);
+    updateChart(x,y, xIndex, yIndex, cars);
     //console.log(x);
 }
-
-//define svg + design elements
-var svg = d3.select('svg');
 
 
 // Get layout parameters
@@ -21,18 +35,19 @@ var svgHeight = +svg.attr('height');
 // Map for referencing min/max per each attribute
 var extentByAttribute = {};
 
-var padding = {t: 50, r: 50, b: 20, l: 20};
+var padding = {t: 50, r: 50, b: 20, l: 50};
 
-var chartWidth = (svgWidth/2-padding.r);
+var chartWidth = (svgWidth/2-padding.r-padding.l);
 var chartHeight = (svgHeight - padding.r);
-
+var customColors = ['#ff5733', '#ffbd69', '#45aaf2', '#2ecc71','#8e44ad','#f7dc6f', '#3498db', '#fd79a8', '#1abc9c', '#f39c12' ];
+var colorScale = d3.scaleOrdinal(customColors);
 
 //the bar will use Y and the scatter will use both!
 //update regularly
 var xScale = d3.scaleLinear().range([0, chartWidth]);
-var yScale = d3.scaleLinear().range([chartHeight , 0]);
+var yScale = d3.scaleLinear().range([chartHeight - 60 , 0]);
 // // axes that are rendered already for you
-var xAxis = d3.axisTop(xScale).ticks(6).tickSize(-chartHeight + 40, 0, 0);
+var xAxis = d3.axisBottom(xScale).ticks(6).tickSize(chartHeight -50, 0, 0);
 var yAxis = d3.axisLeft(yScale).ticks(6).tickSize(-chartWidth, 0, 0);
 
 //var cellPadding = 10;
@@ -74,41 +89,73 @@ d3.csv('cars.csv', dataPreprocessor).then(function(dataset) {
     // Create map for each attribute's extent
     dataAttributes.forEach(function(attribute){
         extentByAttribute[attribute] = d3.extent(dataset, function(d){
+           // console.log(d[attribute]);
             return d[attribute];
         });
     });
 
 
 
-    updateChart('cylinders','economy');
+    updateChart('cylinders','economy (mpg)', 1, 2,cars);
 
 });
 
-    function updateChart(xCat,yCat) {
-         // Pre-render gridlines and labels
+function updateChart(xCat,yCat, xIndex, yIndex, data) {
+  
+        // Pre-render gridlines and labels
 
-        chartA.append('g').call(xAxis).attr('transform', 'translate(0,0)').attr('class', 'x axis');
-        chartB.append('g').call(xAxis).attr('transform', 'translate(0,0)').attr('class', 'x axis');
+    xScale.domain(extentByAttribute[xCat]);
+    yScale.domain(extentByAttribute[yCat]);
 
-        var xALabelText = chartA.append('text');
-        xALabelText.text(xCat).attr('class', 'x axis-label')
-          .attr('transform', function(){ return 'translate('+ (chartWidth/2 - 30) + ', ' + (chartHeight - 10)+ ')'});
+    var toRemove = svg.selectAll('.update');
+    toRemove.remove();
+    
+    chartA.append('g').call(xAxis).attr('transform', 'translate(0,0)').attr('class', 'x axis update');
+    chartB.append('g').call(xAxis).attr('transform', 'translate(0,0)').attr('class', 'x axis update');
+ 
+    var xALabelText = chartA.append('text');
+    xALabelText.text(xCat).attr('class', 'x axis-label update')
+        .attr('transform', function(){ return 'translate('+ (chartWidth/2 - 30) + ', ' + (chartHeight - 10)+ ')'});
 
-        var xBLabelText = chartB.append('text');
-        xBLabelText.text(xCat).attr('class', 'x axis-label')
-          .attr('transform', function(){ return 'translate('+ (chartWidth/2 - 30) + ', ' + (chartHeight - 10)+ ')'});
+    var xBLabelText = chartB.append('text');
+    xBLabelText.text(xCat).attr('class', 'x axis-label update')
+        .attr('transform', function(){ return 'translate('+ (chartWidth/2 - 30) + ', ' + (chartHeight - 10)+ ')'});
 
-        var yALabelText = chartA.append('text');
-        yALabelText.text(yCat).attr('class', 'y axis-label')
-            .attr('transform', 'translate('+[-20, (chartHeight/2 + 20)]+')rotate(270)');
+    var yALabelText = chartA.append('text');
+    yALabelText.text(yCat).attr('class', 'y axis-label update')
+        .attr('transform', 'translate('+[-20, (chartHeight/2 + 20)]+')rotate(270)');
 
-        var yBLabelText = chartB.append('text');
-        yBLabelText.text("count").attr('class', 'y axis-label')
-            .attr('transform', 'translate('+[-20, (chartHeight/2 + 20)]+')rotate(270)');
+    var yBLabelText = chartB.append('text');
+    yBLabelText.text("count").attr('class', 'y axis-label update')
+        .attr('transform', 'translate('+[-20, (chartHeight/2 + 20)]+')rotate(270)');
 
-        //make rectangles
-        //width for each bar will be chartWidth/num of options
 
+    
+    //make rectangles
+    //width for each bar will be chartWidth/num of options
+
+    var dots = chartA.selectAll('.dot')
+        .data(data, function(d){
+            return d.name +'-'+d.year+'-'+d.cylinders; // Create a unique id for the car
+        });
+
+    var dotsEnter = dots.enter()
+        .append('circle')
+        .attr('class', 'dot')
+        .style("fill", function(d) { return colorScale(d[xCat]); })
+        .attr('r', 4);
+
+    dotsEnter.on('mouseover', toolTip.show)
+        .on('mouseout', toolTip.hide);
+
+    dots.merge(dotsEnter).attr('cx', function(d){
+            return xScale(d[xCat]);
+        })
+        .attr('cy', function(d){
+            return yScale(d[yCat]);
+        });
+
+    //dots.exit().remove();
 }
 
 
