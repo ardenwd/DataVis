3c9ee4e6eb167aafe3c731c82f7510900912c30d
ardipi <ardendavis2@gmail.com>
2024-10-15 17:36:23 -0400

    file changes

--- lyrics/spotify_script.py
+++ lyrics/spotify_script.py
@@ -5,8 +5,8 @@ from spotipy.oauth2 import SpotifyClientCredentials
 from bs4 import BeautifulSoup
 import requests
 
-cid ='4ebda78e1f134e1791ca1b3d789b6dff'
-secret ='1ed2dbefd00b45cf900db1fd3cc3f76f'
+cid ='4'
+secret ='s'
 
 client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
 sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
